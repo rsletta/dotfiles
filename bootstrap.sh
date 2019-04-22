b9f6102e910880b9441b7bb549ec7064236d20ec
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2019-04-23 00:26:07 +0200

    SYMLINk .aliases - duh 🤦‍♂️

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,7 @@ function doIt() {
     ln -sf "$PWD"/.bash_profile ~/.bash_profile
     ln -sf "$PWD"/.bashrc ~/.bashrc
     ln -sf "$PWD"/.git-completion.bash ~/.git-completion.bash
+	ln -sf "$PWD"/.aliases ~/.aliases
 	source ~/.bash_profile;
 }
 
