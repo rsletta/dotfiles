4f628ad7afc95c3c379c224ee1b45c5b3757bc23
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2019-04-23 23:35:58 +0200

    Fix bootstrap. Still struggeling.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-function symlink() {
+function doIt() {
     # symlink dotfiles
     ln -sf "$PWD"/.bash_profile ~/.bash_profile
     ln -sf "$PWD"/.bashrc ~/.bashrc
@@ -16,28 +16,23 @@ function symlink() {
 	read -p "Bootstrap environment? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		bootstrap;
+		if [[ "$OSTYPE" == "darwin"* ]]; then
+			# Bootstrap macOS
+			sh macOS.sh;
+		elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+			# Bootstrap Ubuntu
+			sh Ubuntu.sh;
+		fi
 	fi;
 }
 
-function bootstrap() {
-	if [ "`uname`" == "Darwin"] then
-		# Bootstrap macOS
-		sh macOS.sh
-	elif [ "`uname`" == "Linux" ] then
-		# Bootstrap Ubuntu
-		sh Ubuntu.sh
-	fi
-}
-
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	symlink;
+	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		symlink;
+		doIt;
 	fi;
 fi;
-unset symlink;
-unset bootstrap;
+unset doIt;
