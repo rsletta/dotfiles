d50741daf59045b665b9c0dfa5d1a0619cdd540d
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2019-04-27 01:12:42 +0200

    Add Ubuntu support

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -10,3 +10,22 @@ sudo apt upgrade -y
 sudo apt install -y neovim \
                     tmux \
                     mosh
+
+# Set tmux config
+ln -sf "$PWD"/configs/.tmux.conf ~/.tmux.conf
+
+# Set vim config
+ln -sf "$PWD"/configs/.vimrc ~/.vimrc
+ln -sf "$PWD"/configs/nvim ~/.config/nvim                    
+
+# Install NVM
+export NVM_DIR="$HOME/.nvm" && ( git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR" cd "$NVM_DIR" git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`) && \. "$NVM_DIR/nvm.sh"
+
+# Reload config
+reload
+
+# Add SAP NPM registry
+npm config set @sap:registry https://npm.sap.com
+
+# Install global nodeJS tools
+npm install -g typescript yo eslint @ui5/cli @angular/cli @sap/cds @sap/generator-cds
