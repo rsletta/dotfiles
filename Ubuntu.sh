836aae044b342b9e1dfeee6e4064851a6c734ce5
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2019-04-27 01:16:30 +0200

    Add multi line nvm install (fix?)

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -23,7 +23,11 @@ ln -sf "$PWD"/configs/nvim ~/.config/nvim
 
 # Install NVM
 echo "Installing NVM"
-export NVM_DIR="$HOME/.nvm" && ( git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR" cd "$NVM_DIR" git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`) && \. "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
 
 # Reload config
 reload
