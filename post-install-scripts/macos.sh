b11972939b3f208ae6d8a137dd45559aab402ecd
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2021-04-01 21:55:59 +0200

    Add nvm install to macos

--- post-install-scripts/macos.sh
+++ post-install-scripts/macos.sh
@@ -1,15 +1,23 @@
+#!/usr/bin/env bash
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.profile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.extrasrc
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install applications
 brew install bash \
              git \
-             nvm \
              tree \
              ranger \
              tmux \
              neovim \
              figlet
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# Add nvm path to .extrasrc
+echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"' >> ~/.extrasrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.extrasrc
