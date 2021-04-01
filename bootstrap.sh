d248e4e6fe671f898ea07017aeb61ddad0eb159e
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2021-04-02 00:06:08 +0200

    Symlinking clean-up

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,11 +10,21 @@ heading () {
 heading symbolic links
 # Files
 for i in bash_profile bashrc gitconfig vimrc; do
-  echo $i
+  # Remove old symlink
+  if [ -f $HOME/.$i ]; then
+    echo Removing old $HOME/.$i
+    rm .$i
+  fi
+  # Add new symlink
+  echo Adding new $HOME/.$i
   ln -s -f "$dotfiles/$i" "$HOME/.$i"
 done
 
 # Directories
+if [ -d $HOME/.bashrc.d ]; then
+  echo Removing old $HOME/.bashrc.d
+  rm $HOME/.bashrc.d
+fi
 ln -s -f "$dotfiles/bashrc.d" "$HOME/.bashrc.d"
 
 echo Symlinking done
