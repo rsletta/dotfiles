96f1372672b500f8cdd7a46be024c0f357a74d50
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2021-04-02 00:38:09 +0200

    Learning how symlinks work

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,21 +10,12 @@ heading () {
 heading symbolic links
 # Files
 for i in bash_profile bashrc gitconfig vimrc; do
-  # Remove old symlink
-  if [ -f $HOME/.$i ]; then
-    echo Removing old $HOME/.$i
-    rm .$i
-  fi
   # Add new symlink
   echo Adding new $HOME/.$i
-  ln -s -f "$dotfiles/$i" "$HOME/.$i"
+  ln -s -f -F "$dotfiles/$i" "$HOME/.$i"
 done
 
 # Directories
-if [ -d $HOME/.bashrc.d ]; then
-  echo Removing old $HOME/.bashrc.d
-  rm $HOME/.bashrc.d
-fi
-ln -s -f "$dotfiles/bashrc.d" "$HOME/.bashrc.d"
+ln -s -f -F -h "$dotfiles/bashrc.d" "$HOME/.bashrc.d"
 
 echo Symlinking done
