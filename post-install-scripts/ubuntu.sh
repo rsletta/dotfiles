3b5c84cddf89d5a2675a1449bf4200b57b622386
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2021-04-01 22:34:16 +0200

    Install nvm with git

--- post-install-scripts/ubuntu.sh
+++ post-install-scripts/ubuntu.sh
@@ -8,9 +8,17 @@ sudo apt install -y git \
                     neovim \
                     figlet
 
-# Install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# Install nvm using git 
+git clone https://github.com/nvm-sh/nvm.git ~/.nvm
+cd ~/.nvm
+LATEST_NVM=$(curl --silent "https://api.github.com/repos/nvm-sh/nvm/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')  
+git checkout $LATEST_NVM
+. ./nvm.sh
 
-# Add nvm path to .profile
-echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"' >> ~/.extrasrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.extrasrc
+# Add nvm path to .extrasrc
+echo 'export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.extrasrc 
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion' >> ~/.extrasrc
+
+# Return to HOME
+cd $HOME
