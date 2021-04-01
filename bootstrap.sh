31cea3400dac623ebd1892b17481545bf9ced171
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2021-04-01 15:03:39 +0200

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Symlink dotfiles
+ln -sf "$PWD"/bash_profile ~/.bash_profile
+ln -sf "$PWD"/bashrc ~/.bashrc
+ln -sf "$PWD"/git-completion.bash ~/.git-completion.sh
+ln -sf "$PWD"/git-completion.bash ~/.git-completion.sh
