83c9f984e37ddcbaab4f6ef47576639d67595471
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2021-04-02 01:38:27 +0200

    Add some more stuff

--- post-install-scripts/macos.sh
+++ post-install-scripts/macos.sh
@@ -14,7 +14,8 @@ brew install bash \
              tmux \
              neovim \
              fzf \
-             figlet 
+             jq \
+             figlet
 
 # Symlink Neovim config
 if [ ! -d $HOME/.config ]; then
@@ -24,16 +25,16 @@ fi
 echo Adding new Neovim config directory symlink
 ln -s -f -F -h "$dotfiles/nvim" "$HOME/.config/nvim"
 
-# Install nvm using git 
+# Install nvm using git
 git clone https://github.com/nvm-sh/nvm.git ~/.nvm
 cd ~/.nvm
-LATEST_NVM=$(curl --silent "https://api.github.com/repos/nvm-sh/nvm/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')  
+LATEST_NVM=$(curl --silent "https://api.github.com/repos/nvm-sh/nvm/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 git checkout $LATEST_NVM
 . ./nvm.sh
 
 # Add nvm path to .extrasrc
 echo 'export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.extrasrc 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.extrasrc
 echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion' >> ~/.extrasrc
 
 # Return to HOME
--- post-install-scripts/ubuntu.sh
+++ post-install-scripts/ubuntu.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-sudo apt update 
+sudo apt update
 sudo apt install -y git \
                     tree \
                     ranger \
                     tmux \
                     neovim \
                     fzf \
+                    jq \
                     figlet
 
 # Symlink Neovim config
@@ -17,16 +18,16 @@ fi
 echo Adding new Neovim config directory symlink
 ln -s -f -F -h "$dotfiles/nvim" "$HOME/.config/nvim"
 
-# Install nvm using git 
+# Install nvm using git
 git clone https://github.com/nvm-sh/nvm.git ~/.nvm
 cd ~/.nvm
-LATEST_NVM=$(curl --silent "https://api.github.com/repos/nvm-sh/nvm/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')  
+LATEST_NVM=$(curl --silent "https://api.github.com/repos/nvm-sh/nvm/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 git checkout $LATEST_NVM
 . ./nvm.sh
 
 # Add nvm path to .extrasrc
 echo 'export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.extrasrc 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.extrasrc
 echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion' >> ~/.extrasrc
 
 # Return to HOME
