63d69535e96471240e4079b11fa7446065e3ccb7
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2021-04-03 20:11:41 +0200

    Add newsboat install Ubuntu

--- post-install-scripts/ubuntu.sh
+++ post-install-scripts/ubuntu.sh
@@ -7,6 +7,7 @@ sudo apt install -y git \
                     tmux \
                     neovim \
                     kitty \
+                    newsboat \
                     fzf \
                     jq \
                     figlet
