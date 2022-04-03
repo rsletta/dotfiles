8a81fce849ac0acb862e360c9ac24e4999c33c0b
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2022-04-04 00:44:35 +0200

    Add linuxbrew to path

--- bashrc.d/10-path.sh
+++ bashrc.d/10-path.sh
@@ -1,4 +1,6 @@
 export PATH=$PATH:$HOME/.dotfiles/scripts
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv) 
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  export PATH=/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH
+  eval $(/bin/brew shellenv)
 fi
