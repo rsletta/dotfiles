1574b1dc7987c92f801e739be198f6acc59da3f6
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2022-04-20 15:54:12 +0200

    Cleanup

--- bash/.bashrc.d/30-prompt.sh
+++ bash/.bashrc.d/30-prompt.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-# Git stuff
-export GIT_PS1_SHOWCOLORHINTS='y'
-export GIT_PS1_SHOWDIRTYSTATE='y'
-export GIT_PS1_SHOWUNTRACKEDFILES='y'
-export GIT_PS1_SHOWSTASHSTATE="y"
-export GIT_PS1_DESCRIBE_STYLE='describe'
-export GIT_PS1_SHOWUPSTREAM='auto'
-export GIT_PS1_STATESEPARATOR='|'
-
-. "$HOME/.git-completion/git-prompt.sh"
-
-### Prompt ###
 export LC_ALL=
 export CLICOLOR=1
 # Set colors with vivid
