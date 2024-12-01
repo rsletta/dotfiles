2437b541890b0e0887aa9f1c81408b1da9c552ba
Ronnie André Bjørvik Sletta <rsletta@Wheeljack.local>
2024-12-01 13:00:05 +0100

    circles are circles

--- config/30-prompt.sh
+++ config/30-prompt.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-eval "$(oh-my-posh prompt init zsh --config "$HOME/.config/dotfiles/oh-my-posh/.ohmygruvbox.omp.yaml")"
+eval "$(oh-my-posh prompt init zsh --config "$HOME/.config/dotfiles/oh-my-posh/oh-my-rikosjett.yaml")"
