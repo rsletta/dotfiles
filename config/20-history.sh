22ed3c0ab2079b13612d0766c7d5ecd62c366cde
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2024-12-02 01:15:29 +0100

    doing stuff

--- config/20-history.sh
+++ config/20-history.sh
@@ -1,12 +1,15 @@
-# Ignore duplicate commands and commands starting with a space
-HISTIGNORE="&:[ ]*"
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
 
-# Set the history size (number of commands to remember)
-HISTSIZE=1000
-SAVEHIST=2000
+HISTSIZE=5000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+HISTDUPS=erase
 
-# Append to (don't overwrite) the history file
-setopt APPEND_HISTORY
-setopt SHARE_HISTORY
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_SPACE
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
